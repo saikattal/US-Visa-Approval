26e43180c6337eb1aa25532bd706b758e6017b23
Saikat T <saikat07taluk@gmail.com>
2024-06-21 22:16:42 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='us_visa',
+    version='0.0.0',
+    description='US Visa Approval Prediction',
+    author='Saikat T',
+    author_email='saikat07taluk@gmail.com',
+    packages=find_packages(),
+)
